d6aecc3f5dab471a20829211c5d19e12e09a1caa
cavitoriav <cassiavitoriavieira@gmail.com>
2023-04-24 20:05:40 -0300

    qualquer coisa qualquer coisa qualquer coisa

---
+++ fs/src/mes.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class mes {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int mes;
+        System.out.println("Entre com um mês em número:"); 
+        mes = leia.nextInt();
+        if ( mes == 1 ){
+            System.out.println("O mês informado é Janeiro!");
+         }
+        else if( mes == 2 ){
+        System.out.println("O mês informado é Fevereiro!");
+        }
+        if( mes == 3 ){
+        System.out.println("O mês informado é Março!");
+        }
+        if( mes == 4 ){
+        System.out.println("O mês informado é Abril!");
+         }
+        if( mes == 5 ){
+        System.out.println("O mês informado é Maio!");
+        }
+       if( mes == 6 ){
+      System.out.println("O mês informado é Junho!");
+       }
+       if( mes == 7 ){
+        System.out.println("O mês informado é Julho!");
+     }
+    if( mes == 8 ){
+    System.out.println("O mês informado é Agosto!");
+    }
+    if( mes == 9 ){
+    System.out.println("O mês informado é Setembro!");
+    }
+    if( mes == 10 ){
+    System.out.println("O mês informado é Outubro!");
+     }
+    if( mes == 11 ){
+    System.out.println("O mês informado é Novembro!");
+    }
+   if( mes == 12 ){
+  System.out.println("O mês informado é Dezembro!");
+   }
+        }
+    }
---
+++ java/src/inverter.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class inverter {
+    public static void main(String[] args) {
+String frase;
+Scanner leia = new Scanner(System.in);
+String frase = leia.nextLine();
+  }  
+} 
